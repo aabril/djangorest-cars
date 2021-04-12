824965b024b2925857b3d26d4363d4c184d68ba7
albert <albert@bigSur.local>
2021-04-13 00:27:41 +0200

    admin string name

--- cars/models.py
+++ cars/models.py
@@ -5,9 +5,15 @@ class Brand(models.Model):
     brand_name = models.CharField(max_length=30)
     # number_of_cars = 
 
+    def __str__(self):
+        return self.brand_name
+
 class Car(models.Model):
     model_name = models.CharField(max_length=30)
     width = models.FloatField()
     height = models.FloatField()
     # belongs_to = reference to Brand
+
+    def __str__(self):
+        return self.model_name
 
