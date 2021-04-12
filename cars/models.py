c253b9b6d4a942c73b89cb956b4b5d13a97713f8
albert <albert@bigSur.local>
2021-04-13 01:40:17 +0200

    relate car models with brands and makemigration

--- cars/models.py
+++ cars/models.py
@@ -12,8 +12,10 @@ class Model(models.Model):
     model_name = models.CharField(max_length=30)
     width = models.FloatField()
     height = models.FloatField()
-    # belongs_to = reference to Brand
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.model_name
 
+    class Meta:
+        ordering = ['model_name']
