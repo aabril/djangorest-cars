a741559967829a9eb4d95ff810e5f546b7b90b49
albert <albert@bigSur.local>
2021-04-13 00:23:35 +0200

    adds models, migrations and admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Car, Brand
+
+admin.site.register(Car)
+admin.site.register(Brand)
+
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    brand_name = models.CharField(max_length=30)
+    # number_of_cars = 
+
+class Car(models.Model):
+    model_name = models.CharField(max_length=30)
+    width = models.FloatField()
+    height = models.FloatField()
+    # belongs_to = reference to Brand
+
