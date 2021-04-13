8e56ae5093d1599e42529a1be579640499a1ae66
albert <albert@bigSur.local>
2021-04-13 02:11:03 +0200

    optional final slash on urls

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('api/brand/', views.BrandList.as_view()),
-    path('api/brand/<int:pk>/', views.BrandDetail.as_view()),
-    path('api/model/', views.ModelList.as_view()),
-    path('api/model/<int:pk>/', views.ModelDetail.as_view()),
+    path('api/brand/?', views.BrandList.as_view()),
+    path('api/brand/<int:pk>/?', views.BrandDetail.as_view()),
+    path('api/model/?', views.ModelList.as_view()),
+    path('api/model/<int:pk>/?', views.ModelDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
