52146532b90ad6eeaa9ccfbebad3501e9e6e638b
albert <albert@bigSur.local>
2021-04-30 10:45:52 +0200

    Move Brand to ./brands/

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Car, Brand
+from .models import Car
 
 admin.site.register(Car)
-admin.site.register(Brand)
 
--- cars/models.py
+++ cars/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-class Brand(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    brand_name = models.CharField(max_length=30)
-    # number_of_cars = 
-
-    def __str__(self):
-        return self.brand_name
+from brands.models import Brand
 
 class Car(models.Model):
     model_name = models.CharField(max_length=30)
